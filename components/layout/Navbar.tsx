733ac8ad0af336514c2a4788a3924d2ce09a2944
abinfpro <abinkummil@gmail.com>
2026-01-03 10:39:40 +0530

    commit

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -7,10 +7,10 @@ export default async function Navbar() {
   const isAuthenticated = !!token;
 
   return (
-    <nav className="w-full h-16 bg-black/90 backdrop-blur-md text-white border-b border-gray-800 sticky top-0 z-50">
+<nav className="w-full h-16 bg-[#191919] backdrop-blur-md text-white border-b border-gray-800 sticky top-0 z-50">
       <div className="container mx-auto px-4 h-16 flex justify-between items-center">
-        <div className="w-full h-[70px] bg-[#191919] px-[10px]">
-          <div className="h-full flex items-center justify-between px-4 sm:px-6">
+        <div className="w-full h-full">
+          <div className="h-full flex items-center justify-between">
             <img src="/logo.png" alt="" />
             <div className="flex items-center gap-4">
               <NavAuth initialAuth={isAuthenticated} />
